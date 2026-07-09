f25a862b3ce6054b7abeb33012556a1bfc2a1330
Tahiya-296 <tahiya.metro@gmail.com>
2026-07-09 23:08:32 +0600

    Added Chapter 1: Ex-1

---
+++ Chapter-1/ex1.js
@@ -0,0 +1,4 @@
+//1. Print your name, your department, and your university on three separate lines. 
+console.log("Ishrat Jahan Tahiya");
+console.log("Department of CSE");
+console.log("Metropolitan University");
