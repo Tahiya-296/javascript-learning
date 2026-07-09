a2da3db102d312398e411fcba7f9eeecebcf05ff
Tahiya-296 <tahiya.metro@gmail.com>
2026-07-09 23:28:35 +0600

    Added Chapter 1: Ex-3
    Added a console.log line with a syntax error for demonstration.

---
+++ Chapter-1/ex3.js
@@ -0,0 +1,4 @@
+//3. Deliberately remove a quotation mark from a console.log line, run it, and read the error message  carefully.
+
+console.log(Code mein error rakhenge,humari marzi!");
+//SyntaxError
