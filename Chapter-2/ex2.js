8a99356215132ee6b0035876682ec50642689f51
Tahiya-296 <tahiya.metro@gmail.com>
2026-07-10 23:01:59 +0600

    Added Chapter 2: Ex-2
    Add explanation for typeof null behavior in JavaScript.

---
+++ Chapter-2/ex2.js
@@ -0,0 +1,7 @@
+//2. Predict the output of console.log(typeof null) — then run it and search the web for why the  answer is surprising. 
+ console.log(typeof null).
+   
+//output=object
+//typeof null` returns `"object"` because of an old bug in JavaScript that was never fixed for compatibility reasons.
+//when JavaScript was first created, null was accidentally classified as an object internally.
+   
