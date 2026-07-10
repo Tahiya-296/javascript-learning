ba47245d9e33251a7a177804595944e7d7fbbafb
Tahiya-296 <tahiya.metro@gmail.com>
2026-07-10 23:10:07 +0600

    Added Chapter 2: Ex-1

--- Chapter-2/ex1.js
+++ Chapter-2/ex1.js
@@ -1,3 +1,5 @@
+//1. Create const variables for your name and department and a let variable for your semester. Print a  sentence using a template literal. 
+
 const name = "Ishrat Jahan Tahiya";
 const dept = "CSE";
 let semester = 8;
