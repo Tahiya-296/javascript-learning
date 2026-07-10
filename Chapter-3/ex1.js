596e492cb42ed2a623884a065028a9f248c2e39e
Tahiya-296 <tahiya.metro@gmail.com>
2026-07-10 23:50:48 +0600

    Added Chapter 3: Ex-1

---
+++ Chapter-3/ex1.js
@@ -0,0 +1,12 @@
+//1. Write a program that prints whether a number is positive, negative, or zero. 
+const number = -4;
+
+if(number<0){ 
+  console.log("Number is Negative");
+}
+else if(number>0){
+    console.log("Number is Positive");
+}
+else{
+    console.log("Number is Zero");
+}
