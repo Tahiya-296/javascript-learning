d4569c810524d70cfbe6bc97d188eee3bad87968
Tahiya-296 <tahiya.metro@gmail.com>
2026-07-10 23:20:46 +0600

    Added Chapter 2: Ex-4

---
+++ Chapter-2/ex4.js
@@ -0,0 +1,5 @@
+//4. What does "5" + 3 print? What does "5" - 3 print? Run both, then write one sentence explaining  the difference. 
+console.log("5"+3);
+console.log("5"-3);
+
+//"5" + 3 gives 53 because + joins strings, while "5" - 3 gives 2 because - converts "5" to a number and performs subtraction.
