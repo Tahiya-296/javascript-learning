95ba5960f7ce15febe9317e34a4e3bee5dec83a6
Tahiya-296 <tahiya.metro@gmail.com>
2026-07-13 23:52:19 +0600

    Added Chapter 4: Ex-2

---
+++ Chapter-4/ex2.js
@@ -0,0 +1,10 @@
+//2. Compute the sum of numbers from 1 to 100 with a loop, then verify with the formula n(n+1)/2.
+let sum = 0;
+for( let i =1; i<=100; i++){
+ sum = sum +i;
+}
+console.log(sum);
+console.log(100*(100+1)/2);
+
+//5050
+//5050
