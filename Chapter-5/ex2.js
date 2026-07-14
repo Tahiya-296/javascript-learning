4402dfb244ad5efc2279aceccd1196d57a72ff54
Tahiya-296 <tahiya.metro@gmail.com>
2026-07-14 23:29:00 +0600

    Added Chapter 5: Ex-2

---
+++ Chapter-5/ex2.js
@@ -0,0 +1,8 @@
+//2. Write celsiusToFahrenheit(c) using the formula F = C × 9/5 + 32.
+
+function celsiusToFahrenheit(c){
+    return c*9/5 + 32;
+}
+console.log(celsiusToFahrenheit(0)); //32
+console.log(celsiusToFahrenheit(25)); //77
+console.log(celsiusToFahrenheit(100)); //212
