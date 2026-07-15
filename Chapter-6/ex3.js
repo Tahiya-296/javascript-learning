1e4808d905b7b529100358d163410b98781ea14f
Tahiya-296 <tahiya.metro@gmail.com>
2026-07-15 23:44:36 +0600

    Added Chapter 6: Ex-3

---
+++ Chapter-6/ex3.js
@@ -0,0 +1,7 @@
+//3. Use filter to keep only names longer than 4 characters from an array of names. 
+
+const name = ["Alvarez", "Messi", "Romero", "Lily"];
+
+const printName = name.filter((name) => name.length > 4);
+
+console.log(printName);
